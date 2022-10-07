c41fcb207eab45fdf814a37bd4398cd278065cb3
Your Name <moisesjuniorcas20@gmail.com>
2022-10-07 11:16:22 -0500

    conexión a db

--- aplicacion/back/server.js
+++ aplicacion/back/server.js
@@ -1,11 +1,15 @@
 //lo vamos a usar para arrancar el servidor 
 
 const app=require('./app');
+const connectDB=require('./config/database');
 
 //setear el archivo de configuracion
 const dotenv=require('dotenv'); 
 dotenv.config({path:'back/config/config.env'}); //cargamos el archivo de configuracion, path ruta del archivo 
 //para que encuentre los archivos de configuracion 
+
+//conectar a la base de datos
+connectDB(); //llamamos a la funcion de conexion a la base de datos
 
 //declaran metodos en js, llamamos al server
 const server=app.listen(process.env.PORT, () => {
