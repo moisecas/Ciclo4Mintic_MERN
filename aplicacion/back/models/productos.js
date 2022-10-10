48211cce03381e241a0625253ef4fa7723f1c200
Your Name <moisesjuniorcas20@gmail.com>
2022-10-10 16:57:31 -0500

    update

---
+++ aplicacion/back/models/productos.js
@@ -0,0 +1,8 @@
+//modelo schema de productos con sus propiedades y sus validaciones 
+
+const mongoose = require('mongoose'); //importamos mongoose
+
+const productSchema = new mongoose.Schema({
+    
+
+})//creamos el schema, es la estructura de la base de datos
