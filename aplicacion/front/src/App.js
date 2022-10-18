ce6ae8a4a481e07cf4c23665084b5a688d72570a
Your Name <moisesjuniorcas20@gmail.com>
2022-10-18 15:16:23 -0500

    rutas

--- aplicacion/front/src/App.js
+++ aplicacion/front/src/App.js
@@ -2,21 +2,31 @@ import './App.css';
 import React from 'react';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Home from './components/Home';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'; //importar el router del dom
 
 
 
 function App() {
   return (
+    <Router>
+      
+    
     <div className="App">
         <Header />
-        <center>Contenido</center>
-        <center>Contenido</center>
-        <center>Contenido</center>
-        <center>Contenido</center>
-        <center>Contenido</center>
-        <center>Contenido</center>
+        {/* navegacion */}
+        <div className="container container-fluid">
+        <Routes>
+          <Route path={"/"} element={<Home />} /> {/* en la ruta localhost:3000/home o / muestre el componente home ruta de home */}
+          <Route path={"/home"} element={<Home />} />
+        </Routes>
+        </div> 
+        
+        
         <Footer/>
+
     </div>
+    </Router>
   );
 }
 
