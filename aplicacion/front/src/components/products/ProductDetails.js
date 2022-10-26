c7e55a9f8206c36e9a5992dc463d2e9104f2ee6d
Your Name <moisesjuniorcas20@gmail.com>
2022-10-25 20:17:34 -0500

    comentando el código

--- aplicacion/front/src/components/products/ProductDetails.js
+++ aplicacion/front/src/components/products/ProductDetails.js
@@ -12,7 +12,7 @@ export const ProductDetails = () => {
    const {id} =useParams();
    const dispatch= useDispatch();
    //const alert= useAlert();
-   const [quantity, setQuantity] = useState(1)
+   const [quantity, setQuantity] = useState(1) //variable de estado para la cantidad de productos
 
 
    useEffect(() => {
@@ -24,35 +24,34 @@ export const ProductDetails = () => {
 
    }, [dispatch, alert, error, id])
 
-   const increaseQty = () => {
-      const contador = document.querySelector('.count')
+   const increaseQty = () => { //funcion para aumentar la cantidad de productos
+      const contador = document.querySelector('.count') //seleccionamos el elemento con la clase count, me permita subir y bajar el numero de productos
 
-      if (contador.valueAsNumber>=product.inventario) return;
+      if (contador.valueAsNumber>=product.inventario) return; //si el valor del contador es mayor o igual al inventario, no se puede seguir aumentando
 
-      const qty = contador.valueAsNumber+1;
-      setQuantity(qty)
+      const qty = contador.valueAsNumber+1; //si no, se aumenta en 1
+      setQuantity(qty) //se actualiza el estado de la cantidad, asignamos el valor 
    }
 
-   const decreaseQty = () => {
+   const decreaseQty = () => { //funcion para disminuir la cantidad de productos
     const contador = document.querySelector('.count')
 
-    if (contador.valueAsNumber <= 1) return;
-
-    const qty = contador.valueAsNumber-1;
-    setQuantity(qty)
+    if (contador.valueAsNumber <= 1) return; //si el valor del contador es menor o igual a 1, no se puede seguir disminuyendo
+    const qty = contador.valueAsNumber-1; //si no, se disminuye en 1
+    setQuantity(qty) //se actualiza el estado de la cantidad, asignamos el valor
  }
 
   return (
    <Fragment>
     {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
       <Fragment>
-      <MetaData title={product.nombre}></MetaData>
+      <MetaData title={product.nombre}></MetaData> {/*Aqui se pone el titulo de la pagina desde el arreglo*/}
       <div className='row d-flex justify-content-around'>
           <div className='col-12 col-lg-5 img-fluid' id="imagen_producto">
               <Carousel pause='hover'>
-                {product.imagen && product.imagen.map(img =>(
-                  <Carousel.Item key={img.public_id}>
-                    <img className="d-block w-100" src={"../"+img.url} alt={product.nombre}></img>
+                {product.imagen && product.imagen.map(img =>(  //recorremos el arreglo de imagenes que esta dentro del json de la db del producto 
+                  <Carousel.Item key={img.public_id}> {/*Aqui se pone la imagen del arreglo*/} 
+                    <img className="d-block w-100" src={"../"+img.url} alt={product.nombre}></img> {/*Aqui se pone la imagen del arreglo*/}
                   </Carousel.Item>
                 ))}
               </Carousel>
@@ -64,29 +63,29 @@ export const ProductDetails = () => {
               <hr />
 
               <div className='rating-outer'>
-                <div className="rating-inner" style={{width: `${(product.calificacion/5)*100}%`}}></div>
+                <div className="rating-inner" style={{width: `${(product.calificacion/5)*100}%`}}></div> {/* recorro array y traigo aquí la calificacion del producto*/}
               </div>
               <span id="No_de_reviews">  ({product.numCalificaciones} Reviews)</span>
               <hr />
-              <p id="precio_producto">${product.precio}</p>
-              <div className="stockCounter d-inline">
-                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
-                <input type="number" className="form-control count d-inline" value={quantity} readOnly/>
-                <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
+              <p id="precio_producto">${product.precio}</p> {/*Aqui se pone el precio del producto*/}
+              <div className="stockCounter d-inline"> {/*clase stockCounter Aqui se pone el contador de cantidad*/} 
+                <span className="btn btn-danger minus" onClick={decreaseQty}>-</span> {/*simbolo - del carrito, función disminuir*/}
+                <input type="number" className="form-control count d-inline" value={quantity} readOnly/> {/*Aqui se pone el contador de cantidad de productos que va al carrito, el input vacio*/}
+                <span className="btn btn-primary plus" onClick={increaseQty}>+</span> {/*simbolo + del carrito función aumentar*/}
               </div>
-              <button type="button" id="carrito_btn" className="btn btn-primary d-inline ml-4" disabled={product.inventario===0}>Agregar al Carrito</button>
+              <button type="button" id="carrito_btn" className="btn btn-primary d-inline ml-4" disabled={product.inventario===0}>Agregar al Carrito</button> {/*cuando sea 0 inhabilitelo*/}
               <hr />
-              <p>Estado: <span id="stock_stado" className={product.inventario>0 ? 'greenColor':'redColor'}>{product.inventario>0 ? "En existencia": "Agotado"}</span></p>
+              <p>Estado: <span id="stock_stado" className={product.inventario>0 ? 'greenColor':'redColor'}>{product.inventario>0 ? "En existencia": "Agotado"}</span></p> {/*si es mayor que 0 será en existencia verde si no agotado rojo*/}
               <hr />
-              <h4 className="mt-2">Descripción:</h4>
-              <p>{product.descripcion}</p>
+              <h4 className="mt-2">Descripción:</h4> {/*Aqui se pone la descripcion del producto ocupa dos espacios mt-2*/}
+              <p>{product.descripcion}</p> {/*Aqui se pone la descripcion del producto que viene del array que mapeo*/}
               <hr />
               <p id="vendedor">Vendido por: <strong>{product.vendedor}</strong></p>
               <button id="btn_review" type="button" className="btn btn-primary mt-4" 
-              data-toggle="modal" data-target="#ratingModal">Deja tu Opinion</button>
-              <div className="alert alert-danger mt-5" type="alert">Inicia Sesión para dejar tu review</div>
+              data-toggle="modal" data-target="#ratingModal">Dejar opinión</button> {/*ventana emergente con estilo*/}
+              <div className="alert alert-danger mt-5" type="alert">Debes iniciar sesión para dejar un review</div> 
           
-              {/*Mensaje emergente para dejar opinion y calificacion*/}
+              {/*Mensaje emergente opinion y calificacion, clases con espacio y ubicación deseada*/}
               <div className="row mt-2 mb-5">
                 <div className="rating w-50">
                   <div className="modal fade" id="ratingModal" tabIndex="-1" role="dialog"
@@ -99,7 +98,7 @@ export const ProductDetails = () => {
                             <span aria-hidden="true">&times;</span>
                           </button>
                         </div>
-                        <div className="modal-body">
+                        <div className="modal-body">{/*area para los comentarios*/}
                           <ul className="stars">
                             <li className="star"><i className="fa fa-star"></i></li>
                             <li className="star"><i className="fa fa-star"></i></li>
--- aplicacion/front/src/constants/productConstants.js
+++ aplicacion/front/src/constants/productConstants.js
@@ -3,9 +3,9 @@ export const ALL_PRODUCTS_REQUEST = 'ALL_PRODUCTS_REQUEST'; //peticion de produc
 export const ALL_PRODUCTS_SUCCESS = 'ALL_PRODUCTS_SUCCESS'; //peticion de productos exitosa
 export const ALL_PRODUCTS_FAIL = 'ALL_PRODUCTS_FAIL'; //peticion de productos fallida 
 
-export const PRODUCT_DETAILS_REQUEST="PRODUCT_DETAILS_REQUEST"
-export const PRODUCT_DETAILS_SUCCESS="PRODUCT_DETAILS_SUCCESS"
-export const PRODUCT_DETAILS_FAIL="PRODUCT_DETAILS_FAIL"
+export const PRODUCT_DETAILS_REQUEST="PRODUCT_DETAILS_REQUEST" //peticion de detalle de producto
+export const PRODUCT_DETAILS_SUCCESS="PRODUCT_DETAILS_SUCCESS" //peticion de detalle de producto exitosa
+export const PRODUCT_DETAILS_FAIL="PRODUCT_DETAILS_FAIL" //peticion de detalle de producto fallida
 
 
 export const CLEAR_ERRORS = 'CLEAR_ERRORS'; //limpiar errores
