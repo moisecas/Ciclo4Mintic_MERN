e9fd5eab63f75e49077bf12d0bb65f7d01a96650
Your Name <moisesjuniorcas20@gmail.com>
2022-10-28 16:45:07 -0500

    agregando componentes de detalle de producto

--- aplicacion/front/src/App.js
+++ aplicacion/front/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './components/Home';
 import { ProductDetails } from './components/products/ProductDetails';
+import { Dashboard } from './components/admin/Dashboard';
+import ProductList from './components/admin/ProductList';
 
 
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'; //importar el router del dom
@@ -23,7 +25,8 @@ function App() {
           <Route path={"/"} element={<Home />} /> {/* en la ruta localhost:3000/home o / muestre el componente home ruta de home */}
           <Route path={"/home"} element={<Home />} />
           <Route path={"/producto/:id"} element={<ProductDetails />} /> {/* en la ruta localhost:3000/product/:id muestre el componente productDetails ruta de productDetails */}
-          
+          <Route path="/dashboard" element={<Dashboard />}/> {/* en la ruta localhost:3000/dashboard muestre el componente dashboard ruta de dashboard */}
+          <Route path="/productList" element={<ProductList />}/>   {/* en la ruta localhost:3000/productList muestre el componente productList ruta de productList */}
         </Routes>
         </div> 
         
--- aplicacion/front/src/components/layout/Header.js
+++ aplicacion/front/src/components/layout/Header.js
@@ -1,38 +1,47 @@
 import React, { Fragment } from 'react'
-
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <Fragment>
-        <nav className='navbar row'>
-            <div className='col-12 col-md-3'>
-                <div className='navbar-brand'>
-                    <img src="./images/logo.png" alt="TechcenterColombia"></img>
+    <nav className='navbar row'>
+        <div className='col-12 col-md-3'>
+            <div className='navbar-brand'>
+                <Link to="/" ><img src="./images/logo.png" alt="TechcenterColombia"></img></Link>
+            </div>
+        </div>
+
+        <div className='col-12 col-md-6 mt-2 mt-md-0'>
+            <div className="input-group">
+                <input
+                    type="text"
+                    id="search_field"
+                    class="form-control"
+                    placeholder='Que producto busca?'></input>
+                <div class="input-group-append">
+                    <button id="search-btn" class="btn">
+                        <i class="fa fa-search-plus fa-2x text-white" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+            <div className="ml-4 dropdown d-inline">
+                <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button"
+                    id="dropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span>Panel de Control</span></Link>
+                <div className='dropdown-menu' aria-labelledby='dropDownMenu'>
+                    <Link className="dropdown-item" to="/dashboard">Adm. Productos</Link>
+                    <Link className="dropdown-item" to="/">Pedidos</Link>
+                    <Link className="dropdown-item" to="/">Mi cuenta</Link>
+                    <Link className="dropdown-item" to="/">Cerrar Sesion</Link>
                 </div>
             </div>
 
-            <div className='col-12 col-md-6 mt-2 mt-md-0'>
-                <div className="input-group">
-                    <input 
-                        type="text"
-                        id="search_field"
-                        class="form-control"
-                        placeholder='Que producto busca?'></input>
-                        <div class="input-group-append">
-                            <button id="search-btn" class="btn">
-                                <i class="fa fa-search-plus fa-2x text-white" aria-hidden="true"></i>
-                                
-                                </button>    
-                        </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                <span><button type="button" class="btn btn-warning">Iniciar sesión</button></span>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"></i>   
-                <span className="ml-1" id="cart_count">5</span>
-            </div>
+            <Link to="/carrito"><i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
+                <span className="ml-1" id="cart_count">2</span></Link>
+        </div>
 
-        </nav>
+    </nav>
 
     </Fragment>
   )
